7acb75458dccc28b64fefa98d2e5d4f3d07abba9
fabio franci <fabio.franci@gmail.com>
2016-01-19 22:24:13 +0100

    Vai

--- www/js/index.js
+++ www/js/index.js
@@ -1100,6 +1100,18 @@ function onDeviceReady() {
             try {
                 navigator.geolocation.getCurrentPosition(function(position){
                     //success
+                    latitudine_corrente=position.coords.latitude;
+                    longitudine_corrente=position.coords.longitude;
+
+                    alert('Latitude: '          + position.coords.latitude          + '\n' +
+                        'Longitude: '         + position.coords.longitude         + '\n' +
+                        'Altitude: '          + position.coords.altitude          + '\n' +
+                        'Accuracy: '          + position.coords.accuracy          + '\n' +
+                        'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+                        'Heading: '           + position.coords.heading           + '\n' +
+                        'Speed: '             + position.coords.speed             + '\n' +
+                        'Timestamp: '         + position.timestamp                + '\n');
+
                     alert("Success geoloc");
                     latitudine_corrente='100';
                     longitudine_corrente='10';
